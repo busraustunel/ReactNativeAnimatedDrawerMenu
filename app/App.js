e78a0ea73bb56f189a097b2e8ccef6496e5161bb
busraustunel <bussraustunel@gmail.com>
2023-01-20 11:19:30 +0300

    add file structure and react-navigation

---
+++ app/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>App</Text>
+    </View>
+  );
+};
+export default App;
